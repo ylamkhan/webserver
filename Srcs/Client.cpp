772b223cbb8915e3f89a320506ea1f5706bf8440
Yousra Bouzafour <ybouzafo@c4r5p10.1337.ma>
2024-03-01 17:22:59 +0100

    1 - mars -2024

--- Srcs/Client.cpp
+++ Srcs/Client.cpp
@@ -415,7 +415,77 @@ void    send_to_client( std::string cmp_req, std::string  source ,int  sockfd , 
 }
 
 // ///////////////////////////////
+// delete 
+void remove_directory_file(const std::string& name) {
+    DIR *dir;
+    struct dirent *entry;
 
+    dir = opendir(name.c_str());
+    // its a directory
+    if (dir != NULL) {
+        std::cout << "Directory: " << name << std::endl;
+
+        while ((entry = readdir(dir)) != NULL) {
+            std::string entry_name = entry->d_name;
+
+            if (entry_name != "." && entry_name != "..") {
+                std::string full_path = name + "/" + entry_name;
+
+                DIR *sous_directory = opendir(full_path.c_str());
+
+                if (sous_directory != NULL) {
+                    closedir(sous_directory);
+                    // Directory
+                    std::cout << "sous_directoryectory: " << entry_name << std::endl;
+                    remove_directory_file(full_path);
+                } else
+                 {
+                    // File
+                    std::ifstream file(full_path.c_str());
+                    if (file.is_open())
+                     {
+                        file.close();
+                        if (std::remove(full_path.c_str()) == 0) 
+                        {
+                            std::cout << "file deleted: " << entry_name << std::endl;
+                        } 
+                        else 
+                        {
+                            std::cerr << "error delete file: " << entry_name << std::endl;
+                        }
+                    }
+                }
+            }
+        }
+
+        closedir(dir);
+
+        if (rmdir(name.c_str()) == 0) {
+            std::cout << "Directory deleted: " << name << std::endl;
+        } else {
+            std::cerr << "Error deleting directory: " << name << std::endl;
+        }
+    } 
+    else
+     {
+        //not direct , its a file 
+        std::ifstream file(name.c_str());
+        if (file.is_open()) 
+        {
+            file.close();
+            if (std::remove(name.c_str()) == 0) {
+                std::cout << "File deleted: " << name << std::endl;
+            } else {
+                std::cerr << "Error to delete: " << name << std::endl;
+            }
+        }
+         else 
+         {
+            std::cerr << "Error open file  " << name << std::endl;
+        }
+    }
+}
+/////////////////////////////////////
 
 void   Client::handl_methodes()
 {
@@ -605,6 +675,7 @@ void   Client::handl_methodes()
 
         std::string url = "/nfs/homes/ybouzafo/Desktop/aaaaaaaaaaaaaaa/Srcs/" + reqURL;
         std::cout << url << "\n";
+        	remove_directory_file(url);
 
         
         flag_in_out = true;
